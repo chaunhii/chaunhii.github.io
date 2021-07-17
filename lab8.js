4ffca7daa695cb48f1b098c0d9c777fec0c61ffb
chounhie <chounhie@gmail.com>
2021-07-17 19:40:37 +0700

    lab8>

---
+++ lab8.js
@@ -0,0 +1,22 @@
+var car = {
+    color : "red",
+    window : true,
+    carSeat : 4,
+    wheel : 4
+  }
+  var cat = {
+    name : 'Cate',
+    color : ['yellow', 'black'],
+    eyeColor : 'brown',
+  }
+  var car = {
+    color : "red",
+    window : true,
+    carSeat : 4,
+    wheel : 4
+  }
+  var cat = {
+    name : 'Cate',
+    color : ['yellow', 'black'],
+    eyeColor : 'brown',
+  }
